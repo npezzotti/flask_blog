d65bd09e5685844595971b33027bda618ab95cfe
Nathan Pezzotti <npezzotti80@gmail.com>
2021-01-18 19:36:10 -0500

    added auth functions

--- flask-blog/auth.py
+++ flask-blog/auth.py
@@ -6,3 +6,81 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flask-blog.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = "Username is required."
+        elif not password:
+            error = "Password is required."
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,),
+            ).fetch_one() is not None:
+        error = f"User {username} is not registered."        
+        
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+        flash(error)
+    return render_template('auth/register.html')
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+            ).fetch_one()
+        if user is None:
+            error = "Incorrect username."
+        elif not check_password_hash(user['password'], password):
+            error = "Incorrect password."
+        
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+
+            return redirect(url_for('index'))
+        
+        flash(error)
+
+    return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = sessions.get('user_id')
+
+    if user_id is None:
+        g.user_id is None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,),
+            ).fetch_one()
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
