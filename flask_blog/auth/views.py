0e38419d808729fa1869d2a46c70976cfb690612
npezzotti <nathan.pezzotti@datadoghq.com>
2021-03-02 22:02:18 -0500

    separated helper function in auth into utils

--- flask_blog/auth/views.py
+++ flask_blog/auth/views.py
@@ -1,4 +1,3 @@
-import functools
 from flask import (
         Blueprint, flash, g, redirect, render_template, request, session, url_for
         )
@@ -76,13 +75,3 @@ def load_logged_in_user():
 def logout():
     session.clear()
     return redirect(url_for('index'))
-
-def login_required(view):
-    @functools.wraps(view)
-    def wrapped_view(**kwargs):
-        if g.user is None:
-            return redirect(url_for('auth.login'))
-
-        return view(**kwargs)
-
-    return wrapped_view
