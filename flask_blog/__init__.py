39ad76dc7be4f088949acef738f455b4d80ebda0
npezzotti <nathan.pezzotti@datadoghq.com>
2021-03-03 21:06:27 -0500

    fixing lost files

---
+++ flask_blog/__init__.py
@@ -0,0 +1,48 @@
+print("Calling flask_blog init...")
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import config
+
+version_info = (1, 0, 0)
+__version__ = ".".join([str(v) for v in version_info])
+
+db = SQLAlchemy()
+
+def create_app(testing=False):
+    print("Calling create_app in __init__.py...")
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY='default',
+        SQLALCHEMY_TRACK_MODIFICATIONS=False
+    )
+
+    if testing:
+        app.config.from_object(config.TestingConfig)
+    else: 
+        app.config.from_object(config.Config)
+
+    if not app.config.get('SQLALCHEMY_DATABASE_URI'):
+        try:
+            os.makedirs(app.instance_path)
+        except OSError:
+            pass
+        db_path = os.path.join(app.instance_path, 'flask_blog.sqlite')
+        app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
+
+    print(app.config)
+
+    db.init_app(app)
+
+    from flask_blog.cli import init_db_command
+    app.cli.add_command(init_db_command)
+
+    from flask_blog.auth.views import bp as auth_bp
+    app.register_blueprint(auth_bp)
+
+    from flask_blog.blog.views import bp as blog_bp
+    app.register_blueprint(blog_bp)
+    app.add_url_rule('/', endpoint='index')
+
+    return app
---
+++ flask_blog/auth/models.py
@@ -0,0 +1,21 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_blog import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), unique=True, nullable=False)
+
+    @property
+    def password(self):
+        return self.password_hash
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
