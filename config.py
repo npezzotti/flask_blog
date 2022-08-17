aab49de6b7db4bf8b808134f6a6905b2afdd1995
npezzotti <nathan.pezzotti@datadoghq.com>
2022-08-16 23:42:43 -0400

    updating repo

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 class Config:
     """Parent configuration class."""
-    DEBUG = False
     SECRET_KEY = '86e6c98251544abb91729a875fb2badf'
     SQLALCHEMY_DATABASE_URI = 'postgresql://nathan:password@db/flask_blog'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 class TestingConfig(Config):
+    """Testing configuration class"""
     TESTING = True
     SQLALCHEMY_DATABASE_URI = "sqlite://"
